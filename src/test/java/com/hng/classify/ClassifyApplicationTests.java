e254fc5aa8d240e9698f7729ecd9c1da65eff185
Braingenius2 <braingenius2@gmail.com>
2026-04-14 14:48:04 +0100

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/hng/classify/ClassifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hng.classify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
